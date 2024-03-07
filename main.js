b1c2aff90dbb49f2cbcad323cf9cfa1b51b99989
sriganeshnela <161318276+sriganeshnela@users.noreply.github.com>
2024-03-07 12:23:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function change(){
+    document.getElementById("changeborder1").style.border="solid 5px blue";
+    document.getElementById("changeborder2").style.border="solid 5px blue";
+}
+
+function change1(){
+    document.getElementById("changeborder3").style.border="solid 5px blue";
+    document.getElementById("changeborder4").style.border="solid 5px blue";
+}    
+function change2 (){
+    document.getElementById("changeborder5").style.border="solid 5px blue";
+}    
+function change3 (){
+    document.getElementById("changeborder6").style.border="solid 5px blue";
+}    
+function change4 (){
+    document.getElementById("changeborder7").style.border="solid 5px blue";
+}    
